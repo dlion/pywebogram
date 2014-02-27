b56b87f22744e0c0707e910842ba11f7e79c8909
Domenico Leone Luciani <domenicoleoneluciani@gmail.com>
2014-02-27 09:42:00 +0100

    First commit

---
+++ README.md
@@ -0,0 +1,10 @@
+# pyWebogram
+Webogram standalone client
+
+## Description
+[Webogram](https://github.com/zhukov/webogram) is a [Telegram](https://telegram.org/) web client.   
+I'm really tired to use it into my browser so I wrote a simple script using python-webkit to serve js and html files used by Webogram.
+
+## Author
+
+* Domenico Luciani aka DLion
---
+++ pywebogram.py
@@ -0,0 +1,40 @@
+#!/usr/bin/env python3
+
+import argparse
+import os
+import sys
+from gi.repository import Gtk, WebKit
+
+class pyWebogram:
+
+    def __init__(self):
+        self.view = WebKit.WebView()
+
+        settings = self.view.get_settings()
+        settings.set_property('enable-file-access-from-file-uris', True)
+        settings.set_property('enable-webaudio', True)
+        settings.set_property('default-encoding', 'utf-8')
+        settings.set_property('enable-universal-access-from-file-uris', True)
+        
+        self.window = Gtk.Window()
+
+        self.window.set_title('pyWebogram')
+        self.window.connect("delete-event", Gtk.main_quit)
+        self.window.set_icon_from_file(os.path.dirname(os.path.realpath(__file__)) + '/webogram/app/favicon.ico')
+        self.window.add(self.view)
+
+    def _js(self, code):
+        self.view.execute_script(code)
+
+    def run(self):
+        self.view.set_size_request(800, 600)
+        self.window.set_position(Gtk.WindowPosition.CENTER)
+        self.window.show_all()
+        self.view.open(os.path.join('file://', os.path.dirname(os.path.realpath(__file__)), 'webogram/app/index.html'))
+        Gtk.main()
+
+
+if __name__ == '__main__':
+    webo = pyWebogram()
+    webo.run()
+
